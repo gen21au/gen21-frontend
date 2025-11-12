85d5ff7067fdd6202ea13958a020b17d733043ca
Jobayer Hossain <jobayerfx09@gmail.com>
2025-11-12 16:51:33 +0600

    feat(API): update password reset endpoints to include versioning

--- src/utils/api_endpoints.ts
+++ src/utils/api_endpoints.ts
@@ -5,8 +5,8 @@ export const API_ENDPOINTS = {
     //Auth
     LOGIN: '/login?version=2',
     REGISTER: '/register?version=2',
-    FORGOT_PASSWORD: '/password-reset',
-    RESET_PASSWORD: '/submit-password-reset',
+    FORGOT_PASSWORD: '/password-reset?version=2',
+    RESET_PASSWORD: '/submit-password-reset?version=2',
     USER_PROFILE: '/user',
     LOGOUT: '/logout?version=2',
 
